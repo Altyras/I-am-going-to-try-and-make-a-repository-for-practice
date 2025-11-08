9820f0149f9b1bc831081a5540af45c132f54bc3
Alight <dcommando05@gmail.com>
2025-11-08 10:01:37 +0000

    LATEST JAVA FINALLY INSTALLED ON GITHUB, I FINALLY FOUND OUT HOW

--- bitparity.java
+++ bitparity.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
 public static void main(String[] args){
-    
+    Scanner sc = new Scanner(System.in);
+    //I FINALLY GOT JAVA RUNNING IN VSCODE, I DITCHED VSCODE FOR ONLINE ALTERNATIVES LIKE CODECHUM BUT THIS ACTUALLY WORKS!!
+    sc.close();
 }
