b2248503409cf0a30b25a100969c571bb121a1af
Alight <dcommando05@gmail.com>
2025-11-08 10:46:55 +0000

    brb

--- bitparity.java
+++ bitparity.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     //I FINALLY GOT JAVA RUNNING IN VSCODE, I DITCHED VSCODE FOR ONLINE ALTERNATIVES LIKE CODECHUM BUT THIS ACTUALLY WORKS!!
+    int[] a= new int[16];
+    for(int i=0; i<a.length;i++){
+        a[i] = sc.nextInt();
+    }
     sc.close();
 }
